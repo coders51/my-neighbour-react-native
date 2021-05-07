ac8ec3bf1f77d0ac45f1cafe89d6fb3cb85d0ce7
Davide Dall'Olio <ddallolio@coders51.com>
2021-05-07 12:17:58 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import AppTitle from './atoms/AppTitle/AppTitle';
+
+export default () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <View
+        style={{
+          ...styles.body,
+          backgroundColor: isDarkMode ? Colors.black : Colors.white,
+        }}>
+        <AppTitle />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  body: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+  },
+});
