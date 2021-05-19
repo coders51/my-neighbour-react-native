1e7378ebdc594da401f703f57410f41fc14af659
Davide Dall'Olio <ddallolio@coders51.com>
2021-05-19 10:26:01 +0200

    add expo

---
+++ MyNeighbourReactNativeExpo/src/atoms/AppTitle/AppTitle.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import {StyleSheet, Text, useColorScheme} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+export default () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <Text
+      style={{
+        ...styles.text,
+        color: isDarkMode ? Colors.white : Colors.black,
+      }}>
+      My Neighbour React Native
+    </Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 20,
+    fontWeight: '600',
+  },
+});
